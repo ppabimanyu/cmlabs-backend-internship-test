541c2d40be7619afcfaf7a52badea91121007cf1
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2023-11-06 17:39:52 +0700

    initial commit

---
+++ fizzbuzz-test.js
@@ -0,0 +1,10 @@
+function printFizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let output = ''
+        if (i % 3 === 0) output += 'Fizz'
+        if (i % 5 === 0) output += 'Buzz'
+        console.log(output || i)
+    }
+}
+
+printFizzBuzz(100)
